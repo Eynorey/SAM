7430b334250b027911421a54bb06531347dceffd
Adrian Riedel <adrian.riedel@sap.com>
2017-05-18 10:04:42 +0200

    fixed sync.sh

--- _uploads/_USEFUL/sync.sh
+++ _uploads/_USEFUL/sync.sh
@@ -1,12 +1,14 @@
 set old_pwd=$PWD
 eval $(ssh-agent)
+trap "ssh-agent -k" exit
 ssh-add ~/.ssh/id_rsa
-mkdir c:/sam_temp
-cd c:/sam_temp
+mkdir ~/_sam_temp
+cd ~/_sam_temp
 git clone --mirror ssh://eynorey@eynorey.visualstudio.com:22/SAM%20-%20Smartify%20The%20World/_git/sam-private
 cd sam-private.git
 git remote add new-origin git@github.com:Eynorey/SAM.git
 git push new-origin --mirror
+git remote rm new-origin
 cd $old_pwd
 unset old_pwd
-rm -r c:/sam_temp
+rm -R ~/_sam_temp
